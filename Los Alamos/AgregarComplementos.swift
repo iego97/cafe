6ff0cdeca1bc5ec3bc732a79be11a3497b1d79e1
Alumno <ulsa@iMacD112.local>
2018-10-12 07:05:34 -0700

    noche 3.0

--- Los Alamos/AgregarComplementos.swift
+++ Los Alamos/AgregarComplementos.swift
@@ -15,6 +15,46 @@ class AgregarComplementos : UIViewController{
     
     var complementos : [Complemento] = []
     
+    var carrito : Carrito?
+    
+    
+    @IBOutlet weak var imgProductoComplemento: UIImageView!
+    
+    @IBOutlet weak var switchAzucar: UISwitch!
+    @IBOutlet weak var switchStevia: UISwitch!
+    @IBOutlet weak var switchCanela: UISwitch!
+    @IBOutlet weak var switchVainilla: UISwitch!
+    
+    
+    @IBOutlet weak var tapperAzucar: UIStepper!
+    
+    @IBOutlet weak var lblCantidadAzucar: UILabel!
+    
+    @IBAction func doTapAgregarCarrito(_ sender: Any) {
+        
+        carrito = Carrito(nombre: (producto?.nombreProducto)!, precio: (producto?.precio)!, complementos: complementos, imagen: (producto?.imagenProducto)!)
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        
+        imgProductoComplemento.image = producto?.imagenProducto
+        
+        if switchAzucar.isOn == true
+        {
+            tapperAzucar.isHidden = false
+            
+            
+            let nombreComplemento = "Azucar"
+            
+            complementos.append(Complemento(nombre: nombreComplemento, cantidad: tapperAzucar.value))
+            
+            lblCantidadAzucar.text = tapperAzucar.value.description
+            
+            
+        }
+    }
     
     
 }
